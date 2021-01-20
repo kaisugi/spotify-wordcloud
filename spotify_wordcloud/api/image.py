befc5615685b0f781ac18a21d1d091b80d6ee96b
HelloRusk <hellorusk1998@gmail.com>
2021-01-20 19:20:41 +0900

    fix regex
    fix #23

--- spotify_wordcloud/api/image.py
+++ spotify_wordcloud/api/image.py
@@ -36,7 +36,7 @@ def hash_generation(session, text):
 def image_generation(text, ha):
     stopwords = ("THE", "The")
 
-    freq = WordCloud(stopwords=stopwords).process_text(text)
+    freq = WordCloud(stopwords=stopwords, regexp=r"\S[\S']+").process_text(text)
     # force regularize
     for k, v in freq.items():
         if v >= 6:
@@ -47,6 +47,7 @@ def image_generation(text, ha):
         height=576,
         colormap="cool",
         stopwords=stopwords,
+        regexp=r"\S[\S']+"
     ).generate_from_frequencies(freq)
     image = wc.to_image()
     image.save(f"/tmp/{ha}.png", format="png", optimize=True)
